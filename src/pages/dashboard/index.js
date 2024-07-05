4073b42e0d7987dbbaea5ef95169d8bdc2c3fc45
Ibrahim BIDI <Zizwar@users.noreply.github.com>
2024-07-05 20:15:33 +0100

    Update index.js

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -8,3 +8,4 @@ export * from "@/pages/dashboard/scanner";
 //export * from "@/pages/dashboard/charts";
 export * from "@/pages/dashboard/views";
 export * from "@/pages/dashboard/news";
+export * from "@/pages/dashboard/creat-signal";
